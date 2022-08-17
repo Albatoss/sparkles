d9646115ec5e9f128bca51afd818972c3b32c476
Jonathan Cardona <jonathan.cardona@postman.com>
2022-08-17 18:35:05 -0400

    added logging

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,10 +1,13 @@
 Sentry.init do |config|
-  config.dsn = ENV["SENTRY_DSN"]
+  config.dsn = 'https://49e59664c83a44e182d065d3609f7a9f@o1363332.ingest.sentry.io/6655811'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  # Don't use Sentry for performance monitoring; stick to errors.
-  config.traces_sample_rate = 0
-
-  # Capture request bodies; ours don't actually contain any PII.
-  config.send_default_pii = true
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
 end
