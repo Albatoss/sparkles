3badaf10dfcfa475731458976924854e844c6284
Jonathan Cardona <jonathan.cardona@postman.com>
2022-08-17 16:24:53 -0400

    Updated logic for self sparkles

--- app/workers/sparkle_worker.rb
+++ app/workers/sparkle_worker.rb
@@ -1,39 +1,41 @@
+# frozen_string_literal: true
+
 class SparkleWorker < ApplicationWorker
   WORDS_OF_ENCOURAGEMENT = [
-    "Amazing",
-    "Aw yiss",
-    "Awesome",
-    "Bam",
-    "Beautiful",
-    "Boo-yah",
-    "Bravo",
-    "Cheers",
-    "Cool",
-    "Excellent",
-    "Exciting",
-    "Fabulous",
-    "Fantastic",
-    "Good news, everyone",
-    "Great",
-    "Hell yeah",
-    "Hooray",
-    "Oh-ho",
-    "Oh yeah",
-    "Rad",
-    "Rock and roll",
-    "Shut the front door",
-    "Sweet",
-    "Tada",
-    "Whee",
-    "Woah",
-    "Woo",
-    "Woo-hoo",
-    "Woot",
-    "Wow",
-    "Yay",
-    "Yeah",
-    "Yesss",
-    "Yippee"
+    'Amazing',
+    'Aw yiss',
+    'Awesome',
+    'Bam',
+    'Beautiful',
+    'Boo-yah',
+    'Bravo',
+    'Cheers',
+    'Cool',
+    'Excellent',
+    'Exciting',
+    'Fabulous',
+    'Fantastic',
+    'Good news, everyone',
+    'Great',
+    'Hell yeah',
+    'Hooray',
+    'Oh-ho',
+    'Oh yeah',
+    'Rad',
+    'Rock and roll',
+    'Shut the front door',
+    'Sweet',
+    'Tada',
+    'Whee',
+    'Woah',
+    'Woo',
+    'Woo-hoo',
+    'Woot',
+    'Wow',
+    'Yay',
+    'Yeah',
+    'Yesss',
+    'Yippee'
   ].freeze
 
   def perform(options)
@@ -47,10 +49,10 @@ class SparkleWorker < ApplicationWorker
 
       if slack_sparklee.bot?
         text = if slack_sparklee.slack_id == team.sparklebot_id
-          "Aww, thank you, <@#{options[:slack_sparkler_id]}>! That's so thoughtful, but I'm already swimming in sparkles! I couldn't possibly take one of yours, but I apprecate the gesture nonetheless :sparkles:"
-        else
-          "It's so nice that you want to recognize one of my fellow bots! They've all politely declined to join the fun of sparkle hoarding, but I'll pass along your thanks."
-        end
+                 "Aww, thank you, <@#{options[:slack_sparkler_id]}>! That's so thoughtful, but I'm already swimming in sparkles! I couldn't possibly take one of yours, but I apprecate the gesture nonetheless :sparkles:"
+               else
+                 "It's so nice that you want to recognize one of my fellow bots! They've all politely declined to join the fun of sparkle hoarding, but I'll pass along your thanks."
+               end
 
         return team.api_client.chat_postMessage(channel: channel.slack_id, text: text)
       end
@@ -70,11 +72,9 @@ class SparkleWorker < ApplicationWorker
         sparkler.update!(slack_sparkler.attributes)
       end
 
-      # Find the sparklee, adding them to our database if we haven't yet & removing the ability to sparkle yourself
+      # Find the sparklee, adding them to our database if we haven't yet
       sparklee = team.users.find_or_initialize_by(slack_id: options[:slack_sparklee_id])
-      if sparklee != sparkler
-        sparklee.update!(slack_sparklee.attributes) if sparklee.new_record?
-      end
+      sparklee.update!(slack_sparklee.attributes) if sparklee.new_record?
 
       # Determine whether or not we should be showing leaderboard text in our response
       leaderboard_enabled = team.leaderboard_enabled? && sparklee.leaderboard_enabled?
@@ -94,18 +94,17 @@ class SparkleWorker < ApplicationWorker
         permalink: message.permalink
       )
 
-      prefix = WORDS_OF_ENCOURAGEMENT.sample + ("!" * rand(1..3))
-      text = if !leaderboard_enabled
-        "#{prefix} <@#{sparklee.slack_id}> just got a :sparkle:!"
-      elsif sparklee.sparkles.count == 1
-        ":tada: <@#{sparklee.slack_id}> just got their first :sparkle:! :tada:"
-      else
-        "#{prefix} <@#{sparklee.slack_id}> now has #{sparklee.sparkles.count} sparkles :sparkles:"
-      end
-
-      if sparklee == sparkler
-        text += "\n\nWhoa whoa whoa you can't sparkle yourself, <@#{sparkler.slack_id}>!"
-      end
+      prefix = WORDS_OF_ENCOURAGEMENT.sample + ('!' * rand(1..3))
+      text =
+        if sparklee.slack_id == sparkler.slack_id
+          "\n\nWhoa whoa whoa you can't sparkle yourself, <@#{sparkler.slack_id}>!"
+        elsif !leaderboard_enabled
+          "#{prefix} <@#{sparklee.slack_id}> just got a :sparkle:!"
+        elsif sparklee.sparkles.count == 1
+          ":tada: <@#{sparklee.slack_id}> just got their first :sparkle:! :tada:"
+        else
+          "#{prefix} <@#{sparklee.slack_id}> now has #{sparklee.sparkles.count} sparkles :sparkles:"
+        end
 
       team.api_client.chat_postMessage(channel: channel.slack_id, text: text)
 
@@ -118,7 +117,7 @@ class SparkleWorker < ApplicationWorker
     rescue Slack::Web::Api::Errors::UserNotFound
       text = "I couldn't find the teammate you're trying to sparkle :sweat: Make sure you're using a highlighted @mention and that they aren't a guest member!"
       team.api_client.chat_postMessage(channel: channel.slack_id, text: text)
-    rescue
+    rescue StandardError
       # Just re-raise the error if this job has already been retried.
       raise if options[:retried]
 
