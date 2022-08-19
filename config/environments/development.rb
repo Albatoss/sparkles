fb644833166504988f567fa5757d0f0c218eba5a
Bryan Cross <bryanrkcross@gmail.com>
2022-08-19 11:56:02 -0500

    Linting fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,6 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-    # Uncomment if you wish to allow Action Cable access from any origin.
+  # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
